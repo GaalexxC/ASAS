341afd0b6124d5b909a759333940ad8d0f72e3f6
Gary Cornell <gacornell@verizon.net>
2017-09-24 01:47:01 -0400

    Comments

--- src/scripts/nginx_stable.sh
+++ src/scripts/nginx_stable.sh
@@ -2,12 +2,13 @@
 # ASAS 2.10
 # @author: GCornell for devCU Software
 # @contact: support@devcu.com
-# Compatibility: Debian Core Systems (Tested on Ubuntu 12-14-16-17 & Debian 8/9)
-# MAIN: https://www.devcu.com  https://www.devcu.net https://www.exceptionalservers.com
-# REPO: https://github.com/GaryCornell/Auto-Server-Installer
+# Compatibility: Debian Core Systems (Tested on Ubuntu 14x-16x-17x & Debian 8/9)
+# MAIN: https://www.devcu.com
+# CODE: https://github.com/GaalexxC/ASAS
+# REPO: https://www.devcu.net
 # License: GNU General Public License v3.0
 # Created:   06/15/2016
-# Updated:   09/22/2017
+# Updated:   09/24/2017
 
 # Install and/or upgrade Nginx to stable
       echo -e  "\nDo you want to install Nginx Stable(y/n)"
