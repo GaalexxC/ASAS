845ebc31bf43e3f7aaa147bdfa3e3d334c5abab5
Gary Cornell <gacornell@verizon.net>
2017-09-24 00:05:00 -0400

    New Menu
    Removed PHP 5.4/5.5 support due to end of life security updates. Added packages in variables.conf
    
    Added PHP 5.6 and 7.1
    
    Separated php.ini config

--- src/scripts/php_install.sh
+++ src/scripts/php_install.sh
@@ -7,42 +7,88 @@
 # REPO: https://github.com/GaryCornell/Auto-Server-Installer
 # License: GNU General Public License v3.0
 # Created:   06/15/2016
-# Updated:   09/22/2017
+# Updated:   09/24/2017
 
-# Install PHP
+clear
+
+#PHP Install Menu
+
+while [ 4 ]
+do
+
+SELECTPHP=$(
+NEWT_COLORS='
+  root=,blue
+  window=,lightgray
+  border=,white
+  checkbox=black,lightgray
+  actcheckbox=black,blue
+  shadow=,gray
+  button=lightgray,gray
+' \
+whiptail --title "PHP Installer" --radiolist "\nUse up/down arrows and tab to select a PHP version" 15 60 4 \
+        "1)" "PHP 5x" OFF \
+        "2)" "PHP 7x (Recommended)" ON \
+        "3)" "PHP 7.1" OFF \
+        "4)" "Return to Main Menu"  OFF 3>&1 1>&2 2>&3
+)
+
+
+case $SELECTPHP in
+        "1)")
       echo "Do you want to install required PHP applications (Required) (y/n)"
       read CHECKPHP
-   if [ $CHECKPHP == "y" ]; then
+     if [ $CHECKPHP == "y" ]; then
+      echo -e "\nInstalling Required Applications"
+      apt -qq update
+      apt install $PHP5_PACKAGES -y
+      echo -e "\nPHP Installed"
+      echo -e "\nSystem Updated\n"
+     else
+      echo -e "\nSkipping PHP update\n"
+     fi
+      read -p "System is ready for configuration, hit ENTER to continue..."
+      return
+        ;;
+
+        "2)")
+      echo "Do you want to install required PHP applications (Required) (y/n)"
+      read CHECKPHP
+     if [ $CHECKPHP == "y" ]; then
       echo -e "\nInstalling Required Applications"
       apt -qq update
       apt install $PHP7_PACKAGES -y
       echo -e "\nPHP Installed"
       echo -e "\nSystem Updated\n"
-   else
+     else
       echo -e "\nSkipping PHP update\n"
-   fi
-   read -p "System is ready for configuration, hit ENTER to continue..."
+     fi
+      read -p "System is ready for configuration, hit ENTER to continue..."
+      return
+        ;;
 
-# -------
-# PHP INI CONFIG:
-# -------
-   if [ $CHECKPHP == "y" ]; then
-      echo -e "\nSecure PHP INI for FPM/CLI - \nThis will secure the following:\ncgi.fix_pathinfo=0\nexposephp=off\ndisable_functions = disable dangerous stuff"
-      echo -e "\nSecurity Check - Do you want to secure your php.ini? (y/n)"
-      read CHECKPHPINI
-   if [ $CHECKPHPINI == "y" ]; then
-      echo -e "\nMaking backup ups of original fpm and cli php.ini"
-      sudo mv /etc/php/7.0/fpm/php.ini /etc/php/7.0/fpm/php.ini.bak
-      sudo mv /etc/php/7.0/cli/php.ini /etc/php/7.0/cli/php.ini.bak
-      echo -e "\nUpdating fpm and cli php.ini with secure rules"
-      CONFIGFPM=$PHP_FPM_INI
-      cp config/phpini/php.ini $CONFIGFPM 2>/dev/null
-      CONFIGCLI=$PHP_CLI_INI
-      cp config/phpini/php.ini $CONFIGCLI 2>/dev/null
-      echo -e "\nphp.ini fpm and cli secured\n"
-   else
-      echo -e "\nNot a wise choice, We highly suggest securing your php.ini files\n"
-   fi
-   else
-     echo -e "\nSkipping php.ini fpm and cli security\n"
-   fi
+        "3)")
+      echo "Do you want to install required PHP applications (Required) (y/n)"
+      read CHECKPHP
+     if [ $CHECKPHP == "y" ]; then
+      echo -e "\nInstalling Required Applications"
+      apt -qq update
+      apt install $PHP7_PACKAGES -y
+      echo -e "\nPHP Installed"
+      echo -e "\nSystem Updated\n"
+     else
+      echo -e "\nSkipping PHP update\n"
+     fi
+      read -p "System is ready for configuration, hit ENTER to continue..."
+      return
+        ;;
+
+        "4)")
+
+      return
+        ;;
+
+esac
+
+done
+exit
