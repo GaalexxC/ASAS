4c196c6f04092d31dedca023b45e1fd97293e0fc
Gary Cornell <gacornell@verizon.net>
2017-10-02 02:26:24 -0400

    Preamble updated
    palette as well

--- src/scripts/security_tools.sh
+++ src/scripts/security_tools.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-# ASAS 2.10
-# @author: GCornell for devCU Software
-# @contact: gacornell@devcu.com
-# Compatibility: Debian Core Systems (Tested on Ubuntu 14x-16x-17x & Debian 8/9)
-# MAIN: https://www.devcu.com
-# CODE: https://github.com/GaalexxC/ASAS
-# REPO: https://www.devcu.net
-# License: GNU General Public License v3.0
-# Created:   06/15/2016
-# Updated:   09/30/2017
-
+#################################################################################
+#                   *** ASAS 2.10 [Auto Server Admin Script] ***                #
+#        @author: GCornell for devCU Software Open Source Projects              #
+#        @contact: gacornell@devcu.com                                          #
+#        $OS: Debian Core Systems (Tested on Ubuntu 14x -> 17x & Debian 8x/9x)  #
+#        $MAIN: https://www.devcu.com                                           #
+#        $SOURCE: https://github.com/GaalexxC/ASAS                              #
+#        $REPO: https://www.devcu.net                                           #
+#        +Created:   06/15/2016 Ported from nginxubuntu-php7                    #
+#        &Updated:   10/02/2017 02:12 EDT                                       #
+#                                                                               #
+#    This program is free software: you can redistribute it and/or modify       #
+#    it under the terms of the GNU General Public License as published by       #
+#    the Free Software Foundation, either version 3 of the License, or          #
+#    (at your option) any later version.                                        #
+#                                                                               #
+#    This program is distributed in the hope that it will be useful,            #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#    GNU General Public License for more details.                               #
+#                                                                               #
+#    You should have received a copy of the GNU General Public License          #
+#    along with this program.  If not, see http://www.gnu.org/licenses/         #
+#                                                                               #
+#################################################################################
     rebootRequired
 
 echo -e "\nChecking for old kernels\n"
