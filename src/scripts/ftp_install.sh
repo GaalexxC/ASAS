23f4c9684f546cd8afba1ebba40e5deeae46160a
GaalexxC <gacornell@verizon.net>
2017-11-06 04:06:33 -0500

    SELECTFTP Menu

--- src/scripts/ftp_install.sh
+++ src/scripts/ftp_install.sh
@@ -8,7 +8,7 @@
 #        $SOURCE: https://github.com/GaalexxC/ASAS                              #
 #        $REPO: https://www.devcu.net                                           #
 #        +Created:   06/15/2016 Ported from nginxubuntu-php7                    #
-#        &Updated:   10/31/2017 01:37 EDT                                       #
+#        &Updated:   11/06/2017 00:01 EDT                                       #
 #                                                                               #
 #    This program is free software: you can redistribute it and/or modify       #
 #    it under the terms of the GNU General Public License as published by       #
@@ -24,8 +24,82 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/         #
 #                                                                               #
 #################################################################################
-# Install vsFTPd
-     apt install vsftpd -y
+clear
+
+while [ 5 ]
+do
+
+SELECTFTP=$(
+whiptail --title "vsFTPd Installer" --radiolist "\nUse up/down arrows and tab to select a operation\nUpon selection operation will begin without prompts" 20 78 8 \
+        "1)" "Install vsFTPd" ON \
+        "2)" "Configure vsFTPd" OFF \
+        "3)" "Purge vsFTPd (No Config Backup)" OFF \
+        "4)" "Return to Main Menu" OFF \
+        "5)" "Exit" OFF 3>&1 1>&2 2>&3
+)
+
+case $SELECTFTP in
+        "1)")
+      if ! type vsftpd > /dev/null 2>&1; then
+          package() {
+           printf "apt --yes install vsftpd"
+         }
+          systemInstaller
+          vsftpdConfFile
+          ftpver=$(vsftpd -v 0>&1)
+          whiptail --title "vsFTPd Installed" --msgbox "$ftpver installed\nDefault port is 23452 (Run configure vsFTPd to edit)\nPress [Enter] to return to vsFTPd menu" --ok-button "OK" 10 70
+       else
+          ftpver=$(vsftpd -v 0>&1)
+          whiptail --title "vsFTPd Check" --msgbox "$ftpver is already installed\nPress [Enter] to return to vsFTPd menu" --ok-button "OK" 10 70
+      fi
+        ;;
+
+        "2)")
+
+      if type vsftpd > /dev/null 2>&1; then
+         vsftpdport
+         vsftpdipadd6
+         vsftpdRestart
+       else
+         whiptail --title "vsFTPd Check" --msgbox "vsFTPd is not installed\nPress [Enter] to return to vsFTPd menu" --ok-button "OK" 10 70
+      fi
+       ;;
+
+        "3)")
+      if type vsftpd > /dev/null 2>&1; then
+        package() {
+          printf "apt --yes purge vsftpd"
+        }
+        systemInstaller
+        sleep 1
+        pkgcache() {
+           printf "apt-get --yes  autoremove"
+        }
+        updateSources
+        sleep 1
+       else
+         whiptail --title "vsFTPd Check" --msgbox "vsFTPd is not installed\nPress [Enter] to return to vsFTPd menu" --ok-button "OK" 10 70
+      fi
+        ;;
+
+        "4)")
+
+       return
+        ;;
+
+        "5)")
+
+       exit 1
+        ;;
+  esac
+
+ done
+
+exit
+
+
+
+
      echo -e "\nConfigure vsFTPd -  We will use IPv6 enabled"
      echo -n "Enter Port - Something high like 23450 > "
      read VPORT
