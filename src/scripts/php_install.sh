14513da76659838d8eedb2480c4cda301702937e
GaalexxC <gacornell@verizon.net>
2017-11-08 02:43:32 -0500

    Cleanup

--- src/scripts/php_install.sh
+++ src/scripts/php_install.sh
@@ -34,7 +34,7 @@ whiptail --title "PHP Installer" --radiolist "\nUse up/down arrows and space to 
         "1)" "Install PHP 7.2" OFF \
         "2)" "Install PHP 7.1 (Recommended)" ON \
         "3)" "Install PHP 7.0" OFF \
-        "4)" "Install PHP 5.6" OFF \
+        "4)" "Install PHP 5.6 (Deprecated)" OFF \
         "5)" "Configure PHP Settings" OFF \
         "6)" "Backup Config (php.ini)" OFF \
         "7)" "Remove PHP (Config Saved)" OFF \
@@ -46,62 +46,62 @@ whiptail --title "PHP Installer" --radiolist "\nUse up/down arrows and space to 
 case $SELECTPHP in
         "1)")
 
-    if ! type php > /dev/null 2>&1; then
-      phpDependencyCheck
-      package() {
+     if ! type php > /dev/null 2>&1; then
+       phpDependencyCheck
+       package() {
          printf "apt --yes install $PHP72_PACKAGES"
        }
-      systemInstaller
-      completeOperation
-       else
-      phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
-      whiptail --title "PHP Check" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
-   fi
+       systemInstaller
+       completeOperation
+     else
+       phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
+       whiptail --title "PHP Check" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
+     fi
         ;;
 
         "2)")
 
-    if ! type php > /dev/null 2>&1; then
-      phpDependencyCheck
-      package() {
+     if ! type php > /dev/null 2>&1; then
+       phpDependencyCheck
+       package() {
          printf "apt --yes install $PHP71_PACKAGES"
        }
-      systemInstaller
-      completeOperation
-       else
-      phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
-      whiptail --title "PHP Check-Install" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
-   fi
+       systemInstaller
+       completeOperation
+     else
+       phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
+       whiptail --title "PHP Check-Install" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
+     fi
         ;;
 
         "3)")
 
-    if ! type php > /dev/null 2>&1; then
-      phpDependencyCheck
-      package() {
+     if ! type php > /dev/null 2>&1; then
+       phpDependencyCheck
+       package() {
          printf "apt --yes install $PHP70_PACKAGES"
        }
-      systemInstaller
-      completeOperation
-       else
-      phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
-      whiptail --title "PHP Check-Install" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
-   fi
+       systemInstaller
+       completeOperation
+     else
+       phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
+       whiptail --title "PHP Check-Install" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
+     fi
         ;;
 
         "4)")
 
-    if ! type php > /dev/null 2>&1; then
-      phpDependencyCheck
-      package() {
+     if ! type php > /dev/null 2>&1; then
+       phpDependencyCheck
+       package() {
          printf "apt --yes install $PHP56_PACKAGES"
        }
-      systemInstaller
-      completeOperation
-       else
-      phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
-      whiptail --title "PHP Check-Install" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
-   fi
+       systemInstaller
+       completeOperation
+     else
+       phpver=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1)
+       whiptail --title "PHP Check-Install" --msgbox "$phpver is already installed\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
+     fi
         ;;
 
         "5)")
@@ -130,47 +130,83 @@ case $SELECTPHP in
 
         "6)")
 
-   if ! type php > /dev/null 2>&1; then
-        whiptail --title "PHP Check-Install" --msgbox "PHP not installed\nPress [Enter] to continue" --ok-button "OK" 10 70
-   else
-        phpBackupConf
-   fi
+     if ! type php > /dev/null 2>&1; then
+       whiptail --title "PHP Check-Install" --msgbox "PHP not installed\nPress [Enter] to continue" --ok-button "OK" 10 70
+     else
+       phpBackupConf
+     fi
         ;;
 
 
         "7)")
 
-   if ! type php > /dev/null 2>&1; then
-        whiptail --title "PHP Check-Install" --msgbox "PHP not installed\nPress [Enter] to continue" --ok-button "OK" 10 70
-   else
-         sudo apt remove `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
-         sudo apt autoremove
-         rm -rf /etc/php
-         read -p "PHP has been removed, configurations preserved, Press [Enter] to return to main menu"
+   if type php > /dev/null 2>&1; then
+    if (whiptail --title "Remove PHP" --yesno "Warning! Removes PHP (Preserves Configurations)\n\nWould you like to remove PHP" --yes-button "Remove" --no-button "Cancel" 10 70) then
+       package() {
+         printf "apt --yes remove `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`"
+       }
+       systemInstaller
+       sleep 1
+       pkgcache() {
+          printf "apt-get --yes autoremove"
+       }
+       updateSources
+       sleep 1
+       phpPurge
+       sleep 1
+       pkgcache() {
+          printf "apt-get autoclean"
+       }
+       updateSources
+       sleep 1
+         whiptail --title "Purge PHP" --msgbox "PHP has been wiped from system\n\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
+      else
+       cancelOperation
+    fi
+      else
+       whiptail --title "Purge PHP" --msgbox "Nothing to do PHP not installed\nPress [Enter] to continue" --ok-button "OK" 10 70
    fi
         ;;
 
         "8)")
 
-   if ! type php > /dev/null 2>&1; then
-        whiptail --title "PHP Check-Install" --msgbox "PHP not installed\nPress [Enter] to continue" --ok-button "OK" 10 70
+   if type php > /dev/null 2>&1; then
+    if (whiptail --title "Purge PHP" --yesno "Warning! Wipes all traces of PHP from your system!\nAll configurations/logs/repos...etc deleted!\n\nWould you like to purge PHP?" --yes-button "Purge" --no-button "Cancel" 10 70) then
+       package() {
+         printf "apt --yes purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`"
+       }
+       systemInstaller
+       sleep 1
+       pkgcache() {
+          printf "apt-get --yes autoremove"
+       }
+       updateSources
+       sleep 1
+       phpPurge
+       sleep 1
+       pkgcache() {
+          printf "apt-get autoclean"
+       }
+       updateSources
+       sleep 1
+         whiptail --title "Purge PHP" --msgbox "PHP has been wiped from system\n\nPress [Enter] to return to PHP menu" --ok-button "OK" 10 70
+    else
+       cancelOperation
+    fi
    else
-         sudo apt purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
-         sudo apt autoremove
-         rm -rf /etc/php
-         read -p "PHP has been removed from the system, Press [Enter] to return to main menu"
+         whiptail --title "Purge PHP" --msgbox "Nothing to do PHP not installed\nPress [Enter] to continue" --ok-button "OK" 10 70
    fi
         ;;
 
         "9)")
 
-      return
+     return
 
         ;;
 
         "10)")
 
-      exit 1
+     exit 1
 
         ;;
   esac
