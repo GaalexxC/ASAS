2089005aad9e821bf347dd4e933a2ab7467fae48
GaalexxC <gary@devcu.com>
2019-03-25 03:49:23 -0400

    perconaaddrepo()
    percona-release_latest

--- src/lib/mysql.sh
+++ src/lib/mysql.sh
@@ -8,7 +8,7 @@
 #        $SOURCE: https://github.com/GaalexxC/ASAS                              #
 #        $REPO: https://www.devcu.net                                           #
 #        +Created:   06/15/2016 Ported from nginxubuntu-php7                    #
-#        &Updated:   11/28/2018 19:26 EDT                                       #
+#        &Updated:   03/25/2019 03:26 EDT                                       #
 #                                                                               #
 #    This program is free software: you can redistribute it and/or modify       #
 #    it under the terms of the GNU General Public License as published by       #
@@ -94,7 +94,7 @@ perconaaddrepo() {
      wget $PERCONA_MYSQL 2> /dev/null
      sleep 1
      echo -e "XXX\n75\n\nInstalling Percona 5.7 repository packages... \nXXX"
-     dpkg -i percona-release_0.1-6.$(lsb_release -sc)_all.deb 2> /dev/null
+     dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb 2> /dev/null
      FUNC="Repository Percona Server 5.7 installed"
      mysqlDebugLog
      cd $CURDIR
