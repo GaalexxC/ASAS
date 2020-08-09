9dd3b769152a882c3927c67b2935670227a339e0
GaalexxC <gary@devcu.com>
2020-08-09 15:46:26 -0400

    MariaDB MySQL 10.5

--- src/scripts/mysql_install.sh
+++ src/scripts/mysql_install.sh
@@ -91,16 +91,27 @@ case $SELECTMYSQL in
         "2)")
 
      if ! type mysql > /dev/null 2>&1; then
-        whiptail --title "MySQL Check-Install" --msgbox "MySQL not installed" --ok-button "OK" 10 7
-       #phpDependencyCheck
-       #package() {
-       #  printf "apt --yes install $PHP71_PACKAGES"
-       #}
-       #systemInstaller
-       #completeOperation
+       if (whiptail --title "Install MySQL" --yesno "Installing latest MariaDB MySQL Server v10.x\n\nWould you like to continue?" --yes-button "Install" --no-button "Cancel" 10 70) then
+        mariadbaddrepo
+        updateSources
+        mysqlPassword
+        package() {
+         printf "apt --yes install mariadb-server"
+        }
+        systemInstaller
+        mysqlCleanup
+        MYSQLVER=$(mysql -V 2>&1)
+        FUNC="Installed $MYSQLVER"
+        mysqlDebugLog
+        completemessage() {
+         printf "Installed\n$MYSQLVER"
+        }
+        completeOperation
      else
-        dbver=$(mysql -V 2>&1)
-        whiptail --title "MySQL Check-Install" --msgbox "MySQL Installed!\n\n$dbver" --ok-button "OK" 10 7
+        cancelOperation
+     fi
+     else
+        whiptail --title "MySQL Check-Install" --msgbox "MySQL already installed\n$MYSQLVER" --ok-button "OK" 10 70
      fi
         ;;
 
